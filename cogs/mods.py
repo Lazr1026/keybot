2826f8dc011b5c272d8d3d69fb1858368893ffb3
Lazr1026 <lazarm1026@gmail.com>
2022-12-24 18:21:12 -0800

    Add update.

--- cogs/mods.py
+++ cogs/mods.py
@@ -1,8 +1,13 @@
 print("Loading the \"mods\" module.")
+import ast
 import discord
 import os
 import subprocess
+import sys
 from discord.ext import commands
+
+nouser = "No user provided."
+selfcommand = "You cannot run this on yourself."
 
 class mods(commands.Cog):
     def __init__(self, client):
@@ -12,8 +17,11 @@ class mods(commands.Cog):
     @commands.has_guild_permissions(ban_members=True)
     async def ban(self, ctx, member: discord.User = None, reason=""):
         '''Ban Users. Admin+'''
-        if member == None or member == ctx.message.author:
-            await ctx.channel.send("You cannot ban yourself.")
+        if not member:
+            await ctx.channel.send(nouser)
+            return
+        elif member == ctx.message.author:
+            await ctx.channel.send(selfcommand)
             return
         reasonraw = ctx.message.content[28:]
         message = f"You have been banned from {ctx.guild.name} for the reason {reasonraw}"
@@ -26,10 +34,10 @@ class mods(commands.Cog):
     async def probate(self, ctx, member: discord.Member = None):
         '''Locks a user to a single channel. Mods+'''
         if not member:
-            await ctx.channel.send("No user provided.")
+            await ctx.channel.send(nouser)
             return
         elif member == ctx.message.author:
-            await ctx.channel.send("You cannot probate yourself.")
+            await ctx.channel.send(selfcommand)
             return
 
         probateRole = member.guild.get_role(1056074333648338954)
@@ -49,7 +57,7 @@ class mods(commands.Cog):
     async def unprobate(self, ctx, member: discord.Member = None):
         '''Removes a user from probation.  Mods+'''
         if not member:
-            await ctx.channel.send("No user provided.")
+            await ctx.channel.send(nouser)
             return
         elif member is ctx.message.author:
             await ctx.channel.send("Either you are stupid enough to try this or you had no idea what would happen. Most likely the former.")
@@ -65,6 +73,27 @@ class mods(commands.Cog):
                 await ctx.channel.send(f'{member} has been released from the shadow realm.')
             except Exception as e:
                 await ctx.channel.send(f'Exception encountered while unprobating: {e}')
+                
+    @commands.command()
+    async def on_message(self, message: discord.Message):
+        if message.author.name == "GitHub" and message.author.discriminator == "0000":
+            if message.embeds[0].title.startswith("[keybot:master]"):
+                await self.bot.change_presence(status=discord.Status.do_not_disturb)
+                await message.add_reaction("\U0001F6D1")
 
+                subprocess.call(["git", "pull", "origin", "master"])
+                if os.name == "posix":
+                    main_path = f'[\'{sys.argv[0]}\']'
+                elif os.name == "nt":
+                    main_path = f'[\'"{sys.argv[0]}"\']'
+                main_path = ast.literal_eval(main_path)
+
+                await message.add_reaction("\U0000267B")
+                
+                if os.name == "posix":
+                    os.execv(sys.executable, ["python3"] + main_path)
+                if os.name == "nt":
+                    os.execv(sys.executable, ["python"] + main_path)
+        
 async def setup(bot):
     await bot.add_cog(mods(bot))
