85e8326b51809eb4989daaff565630346f046af8
Lazr1026 <lazarm1026@gmail.com>
2023-04-19 22:52:58 -0700

    Fix ban reason only using the first word part 2

--- cogs/mods.py
+++ cogs/mods.py
@@ -23,7 +23,7 @@ class mods(commands.Cog):
         elif member == ctx.message.author:
             await ctx.channel.send(selfcommand)
             return
-        message = f"You have been banned from {ctx.guild.name} for the following reason:\n{reason}"
+        message = f"You have been banned from {ctx.guild.name} for the following reason:\n" + reason
         await member.send(message)
         await ctx.guild.ban(member, reason=reason, delete_message_days=0)
         await ctx.channel.send(f"{member} has been b&. 👍")
