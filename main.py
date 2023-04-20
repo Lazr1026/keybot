989254cfb4c345db8d7d89d27ec6c8560f1bec53
Lazr1026 <lazarm1026@gmail.com>
2023-04-19 22:14:44 -0700

    Add comments, move autoupdating to the main file

--- main.py
+++ main.py
@@ -9,11 +9,32 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 client = commands.Bot(command_prefix=("."), intents=intents)
-client.help_command = commands.DefaultHelpCommand(dm_help=False)
+client.help_command = commands.DefaultHelpCommand(dm_help=True)
 
 home_path = os.path.dirname(os.path.realpath(__file__))
 with open(home_path + "/token") as tokenfile:
     token = tokenfile.read()
+
+@commands.command()
+async def on_message(self, message: discord.Message):
+    if message.author.name == "GitHub" and message.author.discriminator == "0000":
+        if message.embeds[0].title.startswith("[keybot:master]"):
+            await self.bot.change_presence(status=discord.Status.do_not_disturb)
+            await message.add_reaction("\U0001F6D1")
+
+            subprocess.call(["git", "pull", "origin", "master"])
+            if os.name == "posix":
+                main_path = f'[\'{sys.argv[0]}\']'
+            elif os.name == "nt":
+                main_path = f'[\'"{sys.argv[0]}"\']'
+            main_path = ast.literal_eval(main_path)
+
+            await message.add_reaction("\U0000267B")
+
+            if os.name == "posix":
+                os.execv(sys.executable, ["python3"] + main_path)
+            if os.name == "nt":
+                os.execv(sys.executable, ["python"] + main_path)
 
 @client.event
 async def on_ready():
